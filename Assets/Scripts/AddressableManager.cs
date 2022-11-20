36aec72c703ce21e49497e8c708e26f2001ee611
Xinwei Lu <lxinwei7@vt.edu>
2022-11-20 15:07:02 -0800

    added addressable feature (#12)
    integrated addressable feature

--- Assets/Scripts/AddressableManager.cs
+++ Assets/Scripts/AddressableManager.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-
-//public class AssetReferenceVirtualDyno : AssetReferenceT<> {
-//}
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AddressableManager : MonoBehaviour
 {
+    [SerializeField]
+    private AssetReference swordAssetReference;
+
+
+    IResourceLocator swordObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Addressables.InitializeAsync().Completed += AddressablesManager_Completed;
+    }
+
+    private void AddressablesManager_Completed(AsyncOperationHandle<IResourceLocator> obj)
+    {
+        swordAssetReference.InstantiateAsync();
+        swordObject = obj.Result;
     }
 
     // Update is called once per frame
